429473ad9ba487d05176819bf33a7f5eca8606f0
Ravi Nanavati <ravi@matx.com>
2025-12-02 13:22:30 -0800

    Fix the name of the drop dicts trace.

--- src/comp/FlagsDecode.hs
+++ src/comp/FlagsDecode.hs
@@ -435,7 +435,7 @@ traceflags = [
           "trace-conAp",
           "trace-ctxreduce",
           "trace-debug",
-          "trace-drop-dict",
+          "trace-drop-dicts",
           "trace-eq-witnesses",
           "trace-eval-steps",
           "trace-eval-types",
